3f7a14da4008f8c9102c0de52c96db58d7f20806
AnatoliySo <145807212+AnatoliySo@users.noreply.github.com>
2024-07-30 02:22:28 +0300

    Add files via upload

---
+++ Mashin/Mashin.cs
@@ -0,0 +1,46 @@
+﻿namespace Lesson_31
+{
+    public class Mashin
+    {
+        public Inventar _inventar;
+        private int _balance;
+
+        public Mashin(Inventar inventar)
+        {
+            _inventar = inventar;
+            _balance = 0;
+        }
+
+        public void Money(int money)
+        {
+            _balance += money;
+        }
+
+        public int GetChan()
+        {
+            int change = _balance;
+            _balance = 0;
+            return change;
+        }
+        
+        public string PurchaseProduct(int id, int count)
+        {
+            var product = _inventar.GetTovar(id);
+            if (product == null)
+                return "Не понятный товар";
+
+            if (product.PointTovar < count)
+                return "Нехватка товара на складе";
+
+            if (_balance < product.Prise * count)
+            {
+                return "Денег НЕТ";
+            }
+                
+                
+            _balance -= product.Prise * count;
+            product.QuantiUpdate(-count);
+            return $"Товар {product.Name} куплен в количестве {count}.";
+        }
+    }
+}
---
+++ Mashin/Tovar.cs
@@ -0,0 +1,23 @@
+﻿namespace Lesson_31
+{
+    public class Tovar
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Prise { get; set; }
+        public int PointTovar { get; set; }
+
+        public Tovar(int id, string name, int prise, int pointTovar)
+        {
+            Id = id;
+            Name = name;
+            Prise = prise;
+            PointTovar = pointTovar;
+        }
+
+        public void QuantiUpdate(int count)
+        {
+            PointTovar += count ;
+        }
+    }
+}
